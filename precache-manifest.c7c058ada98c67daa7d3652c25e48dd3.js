e7936f5ac1d8eb5e5ea60f29c5e40091ac54cb60
Vladimir <imagen20071@gmail.com>
2022-02-03 11:19:48 +0200

    Updates

---
+++ precache-manifest.c7c058ada98c67daa7d3652c25e48dd3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a3650f6f4052626a0b695586692ae5",
+    "url": "/simpson32133/socialNetwork/index.html"
+  },
+  {
+    "revision": "fc6757359618bfebefcc",
+    "url": "/simpson32133/socialNetwork/static/css/3.c255c60a.chunk.css"
+  },
+  {
+    "revision": "2039c6f98404a0d99057",
+    "url": "/simpson32133/socialNetwork/static/css/4.708a0f81.chunk.css"
+  },
+  {
+    "revision": "87f39dfe19d6ed1352a1",
+    "url": "/simpson32133/socialNetwork/static/css/main.a594c92a.chunk.css"
+  },
+  {
+    "revision": "dae410b1af9a3a38e068",
+    "url": "/simpson32133/socialNetwork/static/js/2.52de0253.chunk.js"
+  },
+  {
+    "revision": "fc6757359618bfebefcc",
+    "url": "/simpson32133/socialNetwork/static/js/3.19e9ce4a.chunk.js"
+  },
+  {
+    "revision": "2039c6f98404a0d99057",
+    "url": "/simpson32133/socialNetwork/static/js/4.deb46a3a.chunk.js"
+  },
+  {
+    "revision": "87f39dfe19d6ed1352a1",
+    "url": "/simpson32133/socialNetwork/static/js/main.880ca497.chunk.js"
+  },
+  {
+    "revision": "ef1eded3016902535acc",
+    "url": "/simpson32133/socialNetwork/static/js/runtime~main.79ae35e9.js"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/simpson32133/socialNetwork/static/media/Spinner-1s-200px.9064981b.svg"
+  }
+]);
